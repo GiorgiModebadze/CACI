74137aa66a5f75e0985444057c750e5e7853c886
Giorgi Modebadze <giorgi9449@gmail.com>
2018-12-02 15:57:35 +0100

    Create FactorAnalysis.R

---
+++ SWP2/FactorAnalysis.R
@@ -0,0 +1,69 @@
+
+rm(list = ls())
+
+library(readxl)
+library(nFactors)
+library(tidyverse)
+library(GPArotation)
+library(psych)
+
+# read Data
+dat <-read_excel("SWP/Data_Chocolate_allinterviews.xlsx", sheet = "AttributeRatingsStacked")
+dat<-as.data.frame(dat)
+str(dat)
+bars<-dat
+summary(dat)
+
+aggregate(dat[,-c(1,2)], by=list(bars$Product),mean, na.rm=TRUE)
+
+library(data.table)
+# use data.table
+bars.dt = as.data.table(bars)
+# melt data.table to "long" format
+bars.dt.long = melt(bars.dt, id.vars = c("Person", "Product"), 
+                    variable.name = "Attribute", value.name = "Value")
+str(bars.dt.long)
+head(bars.dt.long)
+# mean values for each attribute (by each product and attribute)
+bars.dt.long[, Mean.by.prod.att := mean(Value, na.rm = TRUE), by = .(Product, Attribute)]
+head(bars.dt.long)
+# test
+unique(bars.dt.long[, .(Product, Attribute, Mean.by.prod.att)])
+
+
+# impute
+bars.dt.long[is.na(Value), Value := Mean.by.prod.att]
+bars.dt.2 = dcast(bars.dt.long, Person + Product ~ Attribute, value.var = "Value")
+
+## FA 
+
+
+
+# check eigenvalues
+
+eigen(cor(bars.dt.2[,3:15]))$values
+plot(eigen(cor(bars.dt.2[,3:15]))$values)
+
+a.fa. <- fa(bars.dt.2[,3:15],fm="ml", max.iter=1000,SMC=TRUE,scores='Anderson',nfactors=5, 
+          rotate ="varimax")
+
+
+
+
+library(gplots)
+library(RColorBrewer)
+library(semPlot)
+heatmap.2(a.fa.$loadings,col=brewer.pal(9, "Greens"), trace="none", key=FALSE, dend="none",
+          Colv=FALSE, cexCol = 1.0,
+          main="Factor loadings for brand adjectives")
+
+
+
+brand.fa.ob <- factanal(bars.dt.2[,3:15], factors=5, rotation="oblimin")
+semPaths(brand.fa.ob, what="est", residuals=FALSE,
+           cut=0.3, posCol=c("white", "darkgreen"), negCol=c("white", "red"),
+           edge.label.cex=0.75, nCharNodes=7)
+
+
+# PCA BI plot
+
