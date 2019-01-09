a2a9b2936be0c530431e7bc91117c689e9263ed2
Giorgi Modebadze <giorgi9449@gmail.com>
2019-01-09 04:32:17 +0100

    end

--- SWP3/CACISWP/RClustering.R
+++ SWP3/CACISWP/RClustering.R
@@ -7,6 +7,10 @@ selected = bluetooth %>% select(13:26)
 bluetooth
 selected
 
+seg.summ <- function (data , groups) 
+{aggregate (data , list(groups), function (x) mean(as.numeric (x)))}
+
+
 selectedDummy = mlr::createDummyFeatures(selected)
 str(selectedDummy)
 selectedDummy
@@ -21,3 +25,30 @@ a = clusplot(bluetooth, seg.k$cluster, color=TRUE, shade=TRUE,
 
 ggsave("~/Documents/GitHub/CACI/SWP3/CACISWP/clust.png", ,
        dpi = 320, height = 50, width = 100, units = "mm")
+
+
+library(cluster) 
+
+seg.dist = daisy(bluetooth)
+as.matrix(seg.dist)[1:5, 1:5]
+
+seg.hc <- hclust(seg.dist, method="complete")
+plot(seg.hc)
+
+plot(cut(as.dendrogram(seg.hc), h=0.5)$lower[[1]])
+bluetooth[c(478, 561), ]
+bluetooth[c(478,430),]
+
+cor(cophenetic(seg.hc), seg.dist)
+
+
+plot(seg.hc)
+rect.hclust(seg.hc, k=4, border="red")
+
+seg.hc.segment <- cutree(seg.hc, k=4)     # membership vector for 4 groups
+table(seg.hc.segment)
+seg.hc.segment
+seg.hc.segment
+
+
+seg.summ(bluetooth, seg.hc.segment)[,24:37]
