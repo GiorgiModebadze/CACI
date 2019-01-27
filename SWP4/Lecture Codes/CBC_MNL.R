26d3e3d169dbc6d86b11d46a2ef1deeb2eb0eabc
Giorgi Modebadze <giorgi9449@gmail.com>
2019-01-27 18:24:08 +0100

    moveForward

--- SWP4/Lecture Codes/CBC_MNL.R
+++ SWP4/Lecture Codes/CBC_MNL.R
@@ -2,20 +2,20 @@ setwd("C:/Users/klapperd/Dropbox/Humboldt/Lehre/CACI/WS1819/Data")
 #install.packages('mlogit')
 library(mlogit)
 library(data.table)
-
-data.cbc<-read.csv("../cbc_data.csv")
+data.c
+data.cbc<-cleandb
 data.cbc$price<-data.cbc$price/100
 head(data.cbc,8)
 str(data.cbc)
 data_ml_bluetooth <- mlogit.data(data.cbc, choice = "choice", shape = "long",
                                  id.var = "id", alt.var = "alt")
 
-mnl_bluetooth = mlogit(choice ~ -1 +none+price+battery1+battery2+battery3+battery4+weight1+weight2+weight3+
-                         sound1+sound2+sound3, data = data_ml_bluetooth)
+mnl_bluetooth = mlogit(choice ~ -1 + none + price + battery + sound + weight, data = data_ml_bluetooth)
 summary(mnl_bluetooth)
 
 head(data_ml_bluetooth)
 
+ab = as.data.frame(mnl_bluetooth$coefficients)
 
 predict.mnl <- function(model , data ) {
   data.model <- model.matrix(
@@ -26,17 +26,25 @@ predict.mnl <- function(model , data ) {
   cbind (share , data )
 }
 
+mnl_bluetooth$coefficients
+
+data %>% filter(id == 134)
 
 
-new.data<-read.csv("newdata.csv")
-new.data$price<-new.data$price/100
-new.data<-new.data[,-(1:4)]
-head(new.data,12)
-
-predict.mnl(mnl_bluetooth,new.data)
-new.data[,2]<-4
-new.data[4,2]<-0
-new.data
-predict.mnl(mnl_bluetooth,new.data)
+# battery1 	8 hours
+# battery2	10 hours
+# battery3	12 hours
+# battery4    	14 hours
+# 16 hours  (omitted level)
+# 
+# weight1		400 grams
+# weight2		500 grams
+# weight3		600 grams
+# 700 grams (omitted level)
+# 
+# sound1		3.5 stars
+# sound2		4.0 stars
+# sound3		4.5 stars
+# 5.0 stars (omitted level)
 
 
--- SWP4/SWP4/SWP4.R
+++ SWP4/SWP4/SWP4.R
@@ -1,11 +1,65 @@
 library(tidyverse)
 library(MASS)
 
-?TeachingDemos
-data = read.csv("../cbc_data.csv")
-str(data)
 
-xtabs(choice ~ price, data = data)
+data = read.csv("../cbc_data.csv")
+
+
+clean = data %>% filter(!(id %in% a$id))
+
+
+cleandb = clean %>% mutate(battery = case_when(battery1 == 1 ~ "8h",
+                                               battery2 == 1 ~ "10h",
+                                               battery3 == 1 ~ "12h",
+                                               battery4 == 1 ~ "14h",
+                                               TRUE ~ "16h"),
+                           weight = case_when (weight1 == 1 ~ "400g",
+                                               weight2 ==1 ~ "500g",
+                                               weight3 == 1 ~ "600g",
+                                               TRUE ~ "700g"),
+                           sound = case_when(sound1 == 1 ~ "3.5s",
+                                             sound2 == 1 ~ "4.0s",
+                                             sound3 == 1 ~ "4.5s",
+                                             TRUE ~ "5.0s")) %>%
+  dplyr::select(id, cs, alt, choice, none, price, battery, weight, sound) %>%
+  mutate(battery = as.factor(battery), weight = as.factor(weight),
+         sound = as.factor(sound))
+
+str(cleandb)
+
+
+
+str(clean)
+temptb = data %>% filter(choice == 1) %>%  group_by(price) %>%
+  summarise(b1 = sum(battery1),b2 = sum(battery2),b3= sum(battery3),
+                                           b4= sum(battery4), s1= sum(sound1),
+            s2 = sum(sound2), s3 = sum(sound3), w1 = sum(weight1), w2 = sum(weight2),
+            w3 = sum(weight3))
+
+temptb = data %>% filter(choice  ==1 ) %>% mutate(a = paste(price, battery1, battery2,
+                                                            battery3, battery4, weight1,
+                                                            weight2, weight3, sound1,sound2,
+                                                            sound3, " ")) 
+temptb %>% group_by(id, a) %>% summarise( n = n()) %>% arrange(desc(n)) %>% 
+  filter(n == 12)
+
+
+temptb %>% group_by(a) %>% summarise( c = n()) %>% arrange(desc(c))
+
+
+xtabs(choice ~ battery1, data = data)
+xtabs(choice ~ battery2, data = data)
+xtabs(choice ~ battery3, data = data)
+xtabs(choice ~ battery4, data = data)
+
+xtabs(choice ~ sound1, data = data)
+xtabs(choice ~ sound2, data = data)
+xtabs(choice ~ sound3, data = data)
+
+xtabs(choice ~ weight1, data = data)
+xtabs(choice ~ weight2, data = data)
+xtabs(choice ~ weight3, data = data)
+
 
 ## row choice counts
 filter(data, choice==1) %>% count(price)
@@ -18,4 +72,8 @@ cor(data)
 # create long version of data
 head(data,10)
 
-data %>% filter(choice == 1)
+a = data %>% filter(choice == 1) %>% group_by(id, alt) %>% summarise(n = n()) %>% 
+  count(id) %>% arrange(nn)  %>% filter(nn == 1)
+
+
+
